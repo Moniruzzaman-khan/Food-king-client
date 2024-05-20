6c3c3ae00d5d3725b2e5255ef19a4d2c3aa256a0
Moniruzzaman-khan <61977327+Moniruzzaman-khan@users.noreply.github.com>
2024-05-20 08:32:30 +0600

    Update SignUp.jsx

--- src/components/Home/SignUp.jsx
+++ src/components/Home/SignUp.jsx
@@ -9,7 +9,7 @@ const SignUp = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        axios.post("http://localhost:5000/api/v1/registration", user)
+        axios.post("https://food-king-server.vercel.app/api/v1/registration", user)
             .then(result => {
                 console.log(result);
                 navigate("/login")
